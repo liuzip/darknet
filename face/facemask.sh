a7b2881bc1ccc8bf61b0843e8bf83de1b9fd124f
Oliver <zhliu@tecomtech.com>
2018-03-05 09:24:49 +0800

    add face directory
    add face directory from yolo-face:
    https://github.com/imistyrain/yolo-face.git
    facemask:
    https://github.com/imistyrain/facemask.git

---
+++ face/facemask.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../darknet detector demo ./data/ibm.data cfg/yolo-ibm.cfg weights/yolo-ibm_40000.weights
---
+++ face/yolo_face.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../darknet detector demo data/face.data cfg/yolo-face.cfg weights/yolo-face_500000.weights

0d70d61756fd1e3f1726d4ab5f57c31529309e2d
liuzip <liuzip@qq.com>
2018-03-11 18:58:01 +0800

    hidden opencv function draw_train_loss/draw_train_chart

--- src/detector.c
+++ src/detector.c
@@ -24,9 +24,10 @@
 #pragma comment(lib, "opencv_imgproc" OPENCV_VERSION ".lib")
 #pragma comment(lib, "opencv_highgui" OPENCV_VERSION ".lib")
 #endif
-
+#if 0
 IplImage* draw_train_chart(float max_img_loss, int max_batches, int number_of_lines, int img_size);
 void draw_train_loss(IplImage* img, int img_size, float avg_loss, float max_img_loss, int current_batch, int max_batches);
+#endif
 #endif	// OPENCV
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
@@ -96,6 +97,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     args.saturation = net.saturation;
     args.hue = net.hue;
 
+#if 0
 #ifdef OPENCV
 	IplImage* img = NULL;
 	float max_img_loss = 5;
@@ -104,6 +106,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
 	if (!dont_show)
 		img = draw_train_chart(max_img_loss, net.max_batches, number_of_lines, img_size);
 #endif	//OPENCV
+#endif
 
     pthread_t load_thread = load_data(args);
     clock_t time;
@@ -171,10 +174,12 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
         i = get_current_batch(net);
         printf("%d: %f, %f avg, %f rate, %lf seconds, %d images\n", get_current_batch(net), loss, avg_loss, get_current_rate(net), sec(clock()-time), i*imgs);
 
+#if 0
 #ifdef OPENCV
 		if(!dont_show)
 			draw_train_loss(img, img_size, avg_loss, max_img_loss, i, net.max_batches);
 #endif	// OPENCV
+#endif
 
 		//if (i % 1000 == 0 || (i < 1000 && i % 100 == 0)) {
 		if (i % 100 == 0) {
